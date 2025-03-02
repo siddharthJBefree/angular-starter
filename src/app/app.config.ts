98a7473c37330b091a8888614f74fbd8bbbd005a
Siddharth Jaiswal <siddharth.j@befree.com.au>
2025-03-02 18:23:43 +0530

    Commit for pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, isDevMode, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import {provideServiceWorker} from '@angular/service-worker';
 import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ]
 };
